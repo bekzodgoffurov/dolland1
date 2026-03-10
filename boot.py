ba0d37e561d68fb7198d7927afe0b945aaee7c5e
bekzodgoffurov <bekzodgoffurovit@gmail.com>
2026-03-10 16:06:52 +0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -59,7 +59,7 @@ ADMIN_IDS: set[int] = {
 
 DOWNLOAD_DIR = Path("downloads")
 DOWNLOAD_DIR.mkdir(exist_ok=True)
-FFMPEG_PATH    = r"C:\Users\IT PARK\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.0.1-full_build\bin"
+FFMPEG_PATH = "ffmpeg"
 TIKTOK_COOKIES = Path("tiktok_cookies.txt")
 
 # ═══════════════════════════════════════════════════════
@@ -1176,5 +1176,6 @@ if __name__ == "__main__":
         asyncio.run(main())
     except KeyboardInterrupt:
         log.info("⛔ To'xtatildi.")
+
 
 # cmd  pip install curl_cffi
